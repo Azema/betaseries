36ab7434295c3d4b6dd91042b36e381533309d57
Azema <manuel.hervo@gmail.com>
2022-06-25 19:57:05 +0200

    Update Comments.ts Modif de la phrase des évaluations, dans la popup

--- src/Comments.ts
+++ src/Comments.ts
@@ -1167,7 +1167,7 @@ export class CommentsBS implements implRepliesComment {
              */
             let template = `
                 <div class="evaluations">
-                    <div class="size-base">${nbEvaluations} évaluation${nbEvaluations > 1 ? 's': ''} parmis les commentaires et ${this.media.objNote.total} au total</div>
+                    <div class="size-base">${this.media.objNote.total} évaluation${this.media.objNote.total > 1 ? 's': ''} dont ${nbEvaluations} parmis les commentaires</div>
                     <div class="size-base average">Note globale: <strong>${self._parent.objNote.mean.toFixed(2)}</strong></div>
                     <div><table><tbody>`;
             for (let i = 5; i > 0; i--) {
